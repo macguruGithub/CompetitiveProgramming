cdca693e980bc23b9b2394b221296d4c6b75863b
rajaguru <81229088+INMARP1@users.noreply.github.com>
2022-05-13 22:19:18 +0530

    arrays problem 3

---
+++ JavaProject/src/com/practise/rajaguru/arrays/LongestSubStringWithoutRepeatChar_3.java
@@ -0,0 +1,26 @@
+package com.practise.rajaguru.arrays;
+
+public class LongestSubStringWithoutRepeatChar_3 {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        int l = s.length() > 0 ? 1 : 0;
+        String[] sArr = s.split("");
+        for (int i = 0; i < s.length() - 1; i++) {
+            String subStr = sArr[i];
+            for (int j = i + 1; j < s.length(); j++) {
+                if (subStr.contains(sArr[j])) {
+                    break;
+                }
+                subStr = subStr + sArr[j];
+                if (subStr.length() > l)
+                    l = subStr.length();
+            }
+        }
+        return l;
+
+    }
+}
